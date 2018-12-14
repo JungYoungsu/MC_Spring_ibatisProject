6b2dac817ccaeee3bfa21470479924b38d78ca2b
JungYoungsu <terun707@gmail.com>
2018-12-14 11:01:57 +0900

    start

---
+++ src/com/multicampus/biz/board/BoardDAO.java
@@ -0,0 +1,27 @@
+package com.multicampus.biz.board;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class BoardDAO {
+
+	public void insertBoard(BoardVO vo) throws SQLException {
+
+	}
+
+	public void updateBoard(BoardVO vo) throws SQLException {
+
+	}
+
+	public void deleteBoard(BoardVO vo) throws SQLException {
+
+	}
+
+	public BoardVO getBoard(BoardVO vo) throws SQLException {
+		return null;
+	}
+
+	public List<BoardVO> getBoardList(BoardVO vo) throws SQLException {
+		return null;
+	}
+}
